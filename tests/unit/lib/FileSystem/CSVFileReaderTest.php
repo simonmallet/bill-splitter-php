370c4628c539de1433749911b46e0d852e180655
Simon Mallet <simonmallet@Simons-MacBook-Pro.local>
2017-02-14 18:01:52 +1300

    csvfilereader unit tests

--- tests/unit/lib/FileSystem/CSVFileReaderTest.php
+++ tests/unit/lib/FileSystem/CSVFileReaderTest.php
@@ -10,15 +10,24 @@ class CSVFileReaderTest extends TestCase
     /** @var CSVFileReader */
     private $csvFileReader;
 
+    /** @var FileHandler */
+    private $fileHandler;
+
     public function setUp()
     {
-        $fileHandler = Phake::mock(FileHandler::class);
-        $this->csvFileReader = new CSVFileReader($fileHandler);
+        $this->fileHandler = Phake::mock(FileHandler::class);
+        $this->csvFileReader = new CSVFileReader($this->fileHandler);
     }
 
-    public function testOK()
+    public function testGivenAResourceWhenReadIsCalledThenReturnArray()
     {
-        $this->assertTrue(true);
+        $resource = @fopen(dirname(dirname(dirname(__FILE__))).'/data/file.txt', 'r');
+        \Phake::when($this->fileHandler)->getResource()->thenReturn($resource);
+
+        $result = $this->csvFileReader->read();
+
+        \Phake::verify($this->fileHandler)->getResource();
+        $this->assertInternalType('array', $result);
     }
 }
 
